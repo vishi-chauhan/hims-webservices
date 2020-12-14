bebe5019fef45c411cab258f84cc4048bc2cb6a0
Vishwajeet Singh <Vishwajeet Singh@DESKTOP-N6O0G42>
2020-12-14 12:42:15 +0530

    test

--- omod-2.3/src/main/java/org/openmrs/module/webservices/rest/mpi/MPI.java
+++ omod-2.3/src/main/java/org/openmrs/module/webservices/rest/mpi/MPI.java
@@ -37,7 +37,7 @@ import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import org.openmrs.api.context.Context;
-
+//Test
 /*
  * During registration if ADHAR is available enter column-
  * Person Updated = PERNO
